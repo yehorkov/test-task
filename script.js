ac7813015d4dc33e1a372d9ab1f36143cea4e01c
Yehor <102697876+yehorkov@users.noreply.github.com>
2022-10-27 01:12:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 let move = false;
-
 let addRow = function () {
     let rows = document.getElementById("rows"),
         newRow = document.createElement("div"),
-        wordButton = document.createElement('button'),
         userInput = document.getElementsByTagName("input")[0].value;
     rows.append(document.createElement("br"));
-    newRow.classList.add('row');
-    wordButton.classList.add('word-btn');
     newRow.innerHTML = getDynamicRow(userInput);
     rows.append(newRow);
-    newRow.append(wordButton);
 };
 let getDynamicRow = function (userInput) {
     let resultElements = [],
@@ -18,36 +13,68 @@ let getDynamicRow = function (userInput) {
     for (let wordNum = 0; wordNum < words.length; wordNum++) {
         let chars = words[wordNum].split('');
         for (let charNum = 0; charNum < chars.length; charNum++) {
-            rows = resultElements.push(
+            resultElements.push(
                 `<span class="single-char" data-word-num="${wordNum}" data-char-num="${charNum}">${chars[charNum]}</span>`
             );
+
         }
-        console.log(words[wordNum]);
         resultElements.push('<span>&nbsp;</span>');
     }
     return resultElements.join('');
 };
+
 let moveChar = function (event) {
     if (event.type === 'click') {
         if (move) {
-            event.target.removeEventListener('mousemove', moveChar);
-            move = false;
+            let closestChars = document.elementsFromPoint(event.pageX, event.pageY).filter((element) => {
+                return element.classList.contains('single-char') && !element.classList.contains('moving')
+            });
+            if (closestChars.length > 0) {
+                swapCharPosition(document.getElementsByClassName('moving')[0], closestChars[0]);
+                move = false;
+                for (let elem of document.getElementsByClassName('moving')) {
+                    elem.classList.remove('moving');
+                }
+            }
         } else {
+            let coordinates = event.target.getBoundingClientRect();
+            event.target.setAttribute('data-left', coordinates.x);
+            event.target.setAttribute('data-top', coordinates.y);
             event.target.addEventListener('mousemove', moveChar);
+            event.target.classList.add('moving');
             move = true;
         }
     } else if (event.type === 'mousemove') {
         event.target.style.position = 'fixed';
-        event.target.style.left = (event.pageX - 15)+ 'px';
-        event.target.style.top = (event.pageY - 20)+ 'px';
+        event.target.style.left = (event.pageX - 15) + 'px';
+        event.target.style.top = (event.pageY - 15) + 'px';
     }
 };
+
+
+let swapCharPosition = (movingChar, toSwapChar) => {
+    movingChar.setAttribute('ready', false);
+    toSwapChar.setAttribute('ready', false);
+
+    let temp = toSwapChar.outerHTML;
+    movingChar.style.position = '';
+    toSwapChar.outerHTML = movingChar.outerHTML;
+    movingChar.outerHTML = temp;
+    document.querySelectorAll('[ready="false"]').forEach((elem) => {
+        elem.addEventListener('click', moveChar);
+    });
+}
+
+let addEvents = () => {
+    let chars = document.getElementsByClassName('single-char');
+    for (let idx = 0; idx < chars.length; idx++) {
+        chars[idx].addEventListener('click', moveChar);
+        chars[idx].setAttribute('ready', true);
+    }
+}
+
 // Events
 document.querySelector('button').addEventListener('click', (event) => {
     addRow();
-    let chars = document.getElementsByClassName('single-char');
-
-    for (let idx = 0; idx < chars.length; idx++) {
-        chars[idx].addEventListener('click', moveChar);
-    }
+    addEvents();
 });
